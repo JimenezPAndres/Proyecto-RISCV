17fc9a3f1439c3f33f75032ba24459d0cbf3a334
dani-campos-a <159846761+dani-campos-a@users.noreply.github.com>
2024-06-04 11:40:33 -0600

    Update Mux_JALR.v

--- Mux_JALR.v
+++ Mux_JALR.v
@@ -2,7 +2,7 @@ module mux_Jalr(
     input [31:0] PC,
     input [31:0] RS2,
     input Jalr,
-    output out_mux_JALR
+    output [31:0] out_mux_JALR
 );
 
 assign out_mux_JALR = (Jalr) ?  PC: RS2;
