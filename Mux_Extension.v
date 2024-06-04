d437292d96503978a6335ce2c0f63cbb3f772b9b
dani-campos-a <159846761+dani-campos-a@users.noreply.github.com>
2024-06-04 11:40:02 -0600

    Update Mux_Extension.v

--- Mux_Extension.v
+++ Mux_Extension.v
@@ -2,7 +2,7 @@ module mux_extesion(
     input [31:0] extension_signo,
     input [31:0] RS1,
     input ALUSrc,
-    output out_mux_exte
+    output [31:0] out_mux_exte
 );
 
 assign out_mux_exte = (ALUSrc) ?  extension_signo: RS1;
