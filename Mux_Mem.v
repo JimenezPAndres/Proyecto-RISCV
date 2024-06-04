7210cb225dd10358a5b71e05f8726d6a700fd7f5
dani-campos-a <159846761+dani-campos-a@users.noreply.github.com>
2024-06-04 11:41:38 -0600

    Update Mux_Mem.v

--- Mux_Mem.v
+++ Mux_Mem.v
@@ -1,34 +1,14 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/04/2024 09:31:37 AM
-// Design Name: 
-// Module Name: Mux_Mem
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Mux_Mem(
 
     input [31:0] ALU,
-    input [31:0] MEmoria,
+    input [31:0] Memoria,
     input MemtoReg,
-    output out_mux_mem
+    output [31:0] out_mux_mem
 );
 
-assign out_mux_exte = (ALUSrc) ?  Memoria: ALU;
+    assign out_mux_exte = (MemtoReg) ?  Memoria: ALU;
 
 
 
