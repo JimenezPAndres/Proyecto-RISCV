3755a5d20cfae196d819975445cff0cf6ed1562a
dani-campos-a <159846761+dani-campos-a@users.noreply.github.com>
2024-06-05 17:47:43 -0600

    Update Main_Decoder.v

--- Unidad de control/Main_Decoder.v
+++ Unidad de control/Main_Decoder.v
@@ -16,20 +16,21 @@ reg [6:0] output_code;  // 4 bits de salida
 always @* begin
     input_code = {op, F};
     casez(input_code)
-        10'b0110011000: output_code = 7'b0010000;
-        10'b0110011010: output_code = 7'b0011000;
-        10'b0110011111: output_code = 7'b0011000;
-        10'b0110011101: output_code = 7'b0011000;
-
-        10'b0010011010: output_code = 7'b0110001;
-        10'b0010011111: output_code = 7'b0110000;
-        10'b0010011000: output_code = 7'b0110010;
-
-        10'b0100011010: output_code = 7'b1100000;
-
-        10'b0011011zzz: output_code = 7'b0110110;
-
-        10'b0001011zzz: output_code = 7'b0110000;
+        //Tipo R
+        10'b0110011000: output_code = 7'b0010000;//ADD
+        10'b0110011111: output_code = 7'b0011000;//AND
+        10'b0110011101: output_code = 7'b0011000;//SRA
+        10'b0110011100: output_code = 7'b0011000;//XOR
+        //Tipo I
+        10'b0000011010: output_code = 7'b0110001;//LW
+        10'b0010011000: output_code = 7'b0110000;//ADDI
+        10'b1100111000: output_code = 7'b0110010;//JALR
+        //Tipo S
+        10'b0100011010: output_code = 7'b1100000;//SW
+        //Tipo J
+        10'b1101111zzz: output_code = 7'b0110110;//JAL
+        //Tipo U
+        10'b0110111zzz: output_code = 7'b0110000;//LUI
 
         
         default: output_code = 7'b0000000;  
