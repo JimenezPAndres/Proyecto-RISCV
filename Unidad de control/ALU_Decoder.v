a44ea939397f9d33cbba18cd981e998e39cd98ed
JimenezPAndres <144188844+JimenezPAndres@users.noreply.github.com>
2024-06-07 01:39:17 -0600

    Update ALU_Decoder.v

--- Unidad de control/ALU_Decoder.v
+++ Unidad de control/ALU_Decoder.v
@@ -20,55 +20,29 @@
 
 
 module ALU_Decoder(
-
-    input wire ALUD, //salida del main decoder
-
-    input wire [2:0] F, //funct 3
-
-    output wire [1:0] ALUOp //señal de control de la ALU. 
-
+    input wire ALUD,
+    input wire [2:0] F,
+    output wire [1:0] ALUOp 
 );
 
-
-
-    reg [3:0] in_ALU; //unión de F y ALUD
-
-    reg [1:0] output_result; 
-
-
+reg [3:0] in_ALU;
+reg [1:0] output_result;
 
 always @* begin
-
     in_ALU = {ALUD, F};
-
-    casez(in_ALU) //para soportar wildcard (z)
-
-
+    casez(in_ALU)
 
         4'b0zzz: output_result = 2'b00; //ADD
-
         4'b1000: output_result = 2'b00; //ADD
-
         4'b1100: output_result = 2'b01; //XOR
-
         4'b1111: output_result = 2'b10; //AND
-
         4'b1101: output_result = 2'b11; //SRA
 
 
-
-
-
         default: output_result = 4'b0000; // Manejo de caso por defecto
-
     endcase
-
 end
 
-
-
 assign ALUOp = output_result; 
-
-
 
 endmodule
