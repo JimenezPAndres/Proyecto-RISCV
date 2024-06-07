3d9e7e0e9ae946091914f90be5223199b8f154a7
dani-campos-a <159846761+dani-campos-a@users.noreply.github.com>
2024-06-07 01:27:03 -0600

    Update Main_Decoder_tb.v

--- Unidad de control/Main_Decoder_tb.v
+++ Unidad de control/Main_Decoder_tb.v
@@ -1,40 +1,35 @@
+//Objetivo de la prueba:
+//El objetivo de la prueba es verificar que el módulo Main_Decoder genere las señales de 
+//control correctas para diferentes combinaciones de códigos de operación (op) y funciones (F). 
+//Estas señales de control son cruciales para el funcionamiento adecuado del procesador, ya que 
+//determinan cómo deben comportarse otros componentes del sistema en respuesta a las instrucciones
+//decodificadas.
+//Los resultados esperados se comentan al lado de cada prueba realizada. 
+//Estimulos:
+//Se van a inicializar las entradas (op y F) en 0, y se esperan 10 unidades de tiempo
+//Luego se prueban 7 casos diferentes para los cuales se esperan 10 unidades de tiempo entre ellos.
+//Cada conjunto de valores de op y F se mantiene durante 10 unidades de tiempo para permitir la 
+//actualización y estabilización de las señales de salida.
+
 
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/02/2024 03:24:50 PM
-// Design Name: 
-// Module Name: Main_Decoder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
+//Testbench para verificar el comportamiento de las senales de control de la UC
 
 module Main_Decoder_tb(
     );
 // Inputs
-reg [5:0] op;
+reg [6:0] op;
 reg [2:0] F;
 
 // Outputs
-wire ALUD;
-wire RegW;
-wire ALUSrc;
-wire MemW;
-wire Jalr;
-wire PCSrc;
-wire Memtoreg;
+wire ALUD; // ALUDecoder
+wire RegW; // escribir en registro 
+wire ALUSrc; // escoge entre reg e inmediato
+wire MemW; // escribir en memoria
+wire Jalr; //identificar JAL
+wire PCSrc; // mux PC
+wire Memtoreg; // leer de memoria
 
 Main_Decoder prueba (
 .ALUD(ALUD), 
@@ -45,23 +40,24 @@ Main_Decoder prueba (
 .MemW(MemW),
 .Jalr(Jalr),
 .PCSrc(PCSrc),
-.Memtoreg(Memroreg)
+.Memtoreg(Memtoreg)
 );
 
 initial begin
-// Initialize Inputs
-op = 6'b000000;
+// inicializar opcode y function
+op = 7'b000000;
 F = 3'b000;
 
-// Wait for global reset
+// 
 #10;
+
 // Prueba de casos
-op = 6'b100011; F = 3'b010; #10; // Expect ALUD = 0  RegW = 0 ALUSrc = 1   MemW = 1  Jalr=0 PCSrc = 0
-op = 6'b011011; F = 3'b111; #10; // Expect ALUD = 0  RegW = 1 ALUSrc = 1   MemW = 0  Jalr=1 PCSrc = 1
-op = 6'b001011; F = 3'bZZZ; #10; // Expect ALUD = 0  RegW = 1 ALUSrc = 1   MemW = 0  Jalr=0 PCSrc = 0
-op = 6'b110011; F = 3'b010; #10; // Expect ALUD = 1  RegW = 1 ALUSrc = 0   MemW = 0  Jalr=0 PCSrc = 0
-op = 6'b010011; F = 3'b000; #10; // Expect ALUD = 0  RegW = 1 ALUSrc = 1   MemW = 0  Jalr=0 PCSrc = 1
-op = 6'b010011; F = 3'b111; #10; // Expect ALUD = 0  RegW = 1 ALUSrc = 1   MemW = 0  Jalr=0 PCSrc = 0
-op = 6'b110011; F = 3'b000; #10; // Expect ALUD = 1  RegW = 1 ALUSrc = 0   MemW = 0  Jalr=0 PCSrc = 0
+op = 7'b0110011; F = 3'b111; #10; //  ALUD = 0  RegW = 0 ALUSrc = 1   MemW = 1  Jalr=0 PCSrc = 0  Memtoreg=0
+op = 7'b0000011; F = 3'b010; #10; //  ALUD = 0  RegW = 1 ALUSrc = 1   MemW = 0  Jalr=1 PCSrc = 1  Memtoreg=1
+op = 7'b0010011; F = 3'b000; #10; //  ALUD = 0  RegW = 1 ALUSrc = 1   MemW = 0  Jalr=0 PCSrc = 0  Memtoreg=0
+op = 7'b1100111; F = 3'b000; #10; //  ALUD = 1  RegW = 1 ALUSrc = 0   MemW = 0  Jalr=0 PCSrc = 0  Memtoreg=0
+op = 7'b0100011; F = 3'b010; #10; //  ALUD = 0  RegW = 1 ALUSrc = 1   MemW = 0  Jalr=0 PCSrc = 1  Memtoreg=0
+op = 7'b0110111; F = 3'b111; #10; //  ALUD = 0  RegW = 1 ALUSrc = 1   MemW = 0  Jalr=0 PCSrc = 0  Memtoreg=0
+op = 7'b1101111; F = 3'b000; #10; //  ALUD = 1  RegW = 1 ALUSrc = 0   MemW = 0  Jalr=0 PCSrc = 0  Memtoreg=0
 end       
 endmodule
