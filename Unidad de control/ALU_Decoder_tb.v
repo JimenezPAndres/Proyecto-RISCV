03bc876fb5872059681c7486f802e5f0a893a709
dani-campos-a <159846761+dani-campos-a@users.noreply.github.com>
2024-06-07 01:39:05 -0600

    Update ALU_Decoder_tb.v

--- Unidad de control/ALU_Decoder_tb.v
+++ Unidad de control/ALU_Decoder_tb.v
@@ -36,23 +36,15 @@ F = 3'b000;
 #10;
         
 // Prueba de casos
-//ALUD = 0; F = 3'b000; #10; // Expect ALUOp = 00 (ADD)
-//ALUD = 0; F = 3'b001; #10; // Expect ALUOp = 00 (ADD)
-//ALUD = 0; F = 3'b010; #10; // Expect ALUOp = 00 (ADD)
-//ALUD = 0; F = 3'b011; #10; // Expect ALUOp = 00 (ADD)
-//ALUD = 0; F = 3'b100; #10; // Expect ALUOp = 00 (ADD)
-//ALUD = 0; F = 3'b101; #10; // Expect ALUOp = 00 (ADD)
-//ALUD = 0; F = 3'b110; #10; // Expect ALUOp = 00 (ADD)
-//ALUD = 0; F = 3'b111; #10; // Expect ALUOp = 00 (ADD)
+ALUD = 0; F = 3'b000; #10; // Expect ALUOp = 00 (ADD)
+ALUD = 0; F = 3'b100; #10; // Expect ALUOp = 00 (ADD)
+ALUD = 0; F = 3'b101; #10; // Expect ALUOp = 00 (ADD)
+ALUD = 0; F = 3'b111; #10; // Expect ALUOp = 00 (ADD)
 
 ALUD = 1; F = 3'b000; #10; // Expect ALUOp = 00 (ADD)
-ALUD = 1; F = 3'b001; #10; // Expect ALUOp = 01 (XOR)
-ALUD = 1; F = 3'b010; #10; // Expect ALUOp = 10 (AND)
-ALUD = 1; F = 3'b011; #10; // Expect ALUOp = 11 (SRA)
-ALUD = 1; F = 3'b100; #10; // Expect ALUOp = 00 (ADD)
-ALUD = 1; F = 3'b101; #10; // Expect ALUOp = 01 (XOR)
-ALUD = 1; F = 3'b110; #10; // Expect ALUOp = 10 (AND)
-ALUD = 1; F = 3'b111; #10; // Expect ALUOp = 11 (SRA)
+ALUD = 1; F = 3'b100; #10; // Expect ALUOp = 01 (XOR)
+ALUD = 1; F = 3'b101; #10; // Expect ALUOp = 11 (SRA)
+ALUD = 1; F = 3'b111; #10; // Expect ALUOp = 10 (AND)
 
 end
 endmodule
