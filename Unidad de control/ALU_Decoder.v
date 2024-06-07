f520e9d7cea743a028d1b90636fdc58784cdc1df
dani-campos-a <159846761+dani-campos-a@users.noreply.github.com>
2024-06-07 01:11:01 -0600

    Update ALU_Decoder.v

--- Unidad de control/ALU_Decoder.v
+++ Unidad de control/ALU_Decoder.v
@@ -1,3 +1,21 @@
+//Descripción General:
+//El módulo ALU_Decoder es un decodificador que traduce señales de control y códigos de función 
+//en operaciones específicas para la Unidad Lógica Aritmética (ALU). Este módulo toma una señal 
+//de control (ALUD) y un código de función (F),y produce una salida (ALUOp) que indica la operación
+//que la ALU debe realizar.
+//Entradas:
+//ALUD (1 bit): Señal de control que influye en la operación de la ALU.
+//F (3 bits): Código de función que especifica la operación deseada dentro de la ALU
+//Salida:
+//ALUOp (2 bits): Código que indica la operación específica que debe realizar la ALU.
+//Funcionabilidad:
+//El módulo combina las señales de entrada ALUD y F en un solo registro (in_ALU) de 4 bits. Luego, 
+//mediante una sentencia casez, se determina la operación de la ALU basada en el valor de in_ALU. 
+//Dependiendo del valor de este código combinado, se asigna un output_result de 2 bits, que se asigna 
+//a la salida ALUOp.
+
+
+
 module ALU_Decoder(
 
     input wire ALUD,
