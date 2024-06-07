c4fdbe2b811ed8358e69d58c14c2501ccc01d490
dani-campos-a <159846761+dani-campos-a@users.noreply.github.com>
2024-06-07 07:17:21 -0600

    Update Mux_extension_tb.v

--- Mux_extension_tb.v
+++ Mux_extension_tb.v
@@ -1,3 +1,10 @@
+//el objetivo es comprobar que la salida out_mux_exte refleja correctamente las entradas extension_signo y 
+//RS1 según el valor de la señal de control ALUSrc.
+//Se tienen 4 estímulos, con un tiempo de aplicación de 10 ns
+//Resultados esperados:
+//Si ALUSrc = 1, entonces out_mux_exte = extension_signo.
+//Si ALUSrc = 0, entonces out_mux_exte = RS1.
+
 `timescale 1ns / 1ps
 
 module mux_extension_tb;
@@ -21,21 +28,25 @@ module mux_extension_tb;
         RS1 = 32'h7FFFFFFF; 
         ALUSrc = 1;
         #10;
+        //Salida esperada: debería ser igual a extension_signo, es decir, 32'h00000000
         
         extension_signo = 32'hFFFFFFFF; 
         RS1 = 32'h12345678; 
         ALUSrc = 0;
         #10;
+        //Salida esperada: debería ser igual a RS1, es decir, 32'h12345678
         
         extension_signo = 32'hAAAAAAAA; 
         RS1 = 32'h55555555; 
         ALUSrc = 1;
         #10;
+        //Salida esperada: debería ser igual a extension_signo, es decir, 32'hAAAAAAAA
 
         extension_signo = 32'h0000FFFF; 
         RS1 = 32'hFFFF0000; 
         ALUSrc = 0;
         #10;
+        //Salida esperada: debería ser igual a RS1, es decir, 32'hFFFF0000
         
         // Fin de la simulación
         $finish;
